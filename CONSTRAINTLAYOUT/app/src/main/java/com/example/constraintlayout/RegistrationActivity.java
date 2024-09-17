44066d289b4c3e10542ec87f2d9278834e2fac6a
zangtea <167078232+zangtea@users.noreply.github.com>
2024-09-17 22:14:54 +0700

    first commit

---
+++ CONSTRAINTLAYOUT/app/src/main/java/com/example/constraintlayout/RegistrationActivity.java
@@ -0,0 +1,29 @@
+package com.example.constraintlayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_registration);
+        TextView btn = findViewById(R.id.alreadyHaveAccount);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegistrationActivity.this,LoginActivity.class));
+            }
+        });
+    }
+}
